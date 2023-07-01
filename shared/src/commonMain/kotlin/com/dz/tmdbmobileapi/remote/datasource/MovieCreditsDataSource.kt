f9c5586943f843d477e2e74b8011b32231355b79
Umair Ali Khalid <umair.khalid786@outlook.com>
2023-07-01 02:07:40 +0200

    Movie Credit datasource usecase added

--- shared/src/commonMain/kotlin/com/dz/tmdbmobileapi/remote/datasource/MovieCreditsDataSource.kt
+++ shared/src/commonMain/kotlin/com/dz/tmdbmobileapi/remote/datasource/MovieCreditsDataSource.kt
@@ -1,21 +1,21 @@
 package com.dz.tmdbmobileapi.remote.datasource
 
-import com.dz.tmdbmobileapi.base.Constants
+import com.dz.tmdbmobileapi.base.Constants.BASE_URL
 import com.dz.tmdbmobileapi.base.Resource
 import com.dz.tmdbmobileapi.remote.NetworkClient
 import com.dz.tmdbmobileapi.remote.NetworkClient.safeGet
 import com.dz.tmdbmobileapi.remote.Params
-import com.dz.tmdbmobileapi.remote.responses.TvSeriesDetails
+import com.dz.tmdbmobileapi.remote.responses.CreditsResponse
 
 
-class TvSeriesDetailsDataSource(private val network: NetworkClient) {
+class MovieCreditsDataSource(private val network: NetworkClient) {
     suspend fun get(
-        tvSeriesId: Int,
+        movieId: Int,
         apiKey: String = "d31c90f6e90f1993a5cbce70c8c53ce8",
         language: String = "en"
-    ): Resource<TvSeriesDetails> {
+    ): Resource<CreditsResponse> {
 
-        val url = Constants.BASE_URL + "tv/$tvSeriesId"
+        val url = BASE_URL + "movie/$movieId/credits"
 
         return network.httpClient.safeGet(url, Params().apply {
             put("api_key", apiKey)
