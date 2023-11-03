6b44ec348db50d60695bccae510c9b1b69ac776d
Umair Khalid <umair.khalid786@outlook.com>
2023-11-03 23:46:08 +0100

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
     kotlin("multiplatform").version("1.8.21").apply(false)
     id("com.squareup.sqldelight").version("1.5.5").apply(false)
 }
-
+ 
 tasks.register("clean", Delete::class) {
     delete(rootProject.buildDir)
 }
